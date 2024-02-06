b57e6c500a1d4bba7af74bce4b486b00410a27bb
Adrian Degenkolb <ade@itscope.de>
2024-02-06 01:37:09 +0100

    feat: Implement simulation ImuplseSpheres

---
+++ src/main/java/simulations/ImpulseSpheres.kt
@@ -0,0 +1,65 @@
+package simulations
+
+import algebra.Vec
+import framework.interfaces.Status
+import physics.ImpulseConnection
+import physics.PhysicsSimulation
+import physics.Sphere
+import toVec
+import java.awt.Color
+import kotlin.math.PI
+
+/**
+ * We create a new simulation where a ball bounces up and down
+ */
+class ImpulseSpheres: PhysicsSimulation("Bounce") {
+    private lateinit var balls: List<Sphere>
+    private lateinit var anchors: List<Sphere>
+    private lateinit var links: List<ImpulseConnection>
+    init {
+        reset()
+        camera.focalLength = 10.0
+        camera.x = 0.0
+        camera.y = -37.0
+        camera.z = 0.5
+        camera.phi = PI
+        camera.theta = PI / 2
+        camera.focalLength = 10.0
+        camera.zoom = 0.01
+    }
+
+    /**
+     * Calc forces is invoked once per tick and enables us to apply gravity to the ball and invert its direction if needed.
+     */
+    override fun calcForces() {
+        for (ball in balls) {
+            applyGravity(listOf(ball))
+        }
+    }
+
+    /**
+     * Reset the Ball
+     */
+    override fun reset() {
+        super.reset()
+        balls = List(7) {Sphere(it.toDouble(), 0.0, 0.0, 0.5, 1.0)}
+        anchors = List(7) {Sphere(it.toDouble(), 0.0, 4.0, 0.5, 1.0)}
+        anchors.forEach { it.status = Status.Immovable }
+        links = List(7) { ImpulseConnection(balls[it], anchors[it], 4.0, 1000.0) }
+        balls[0].set(Vec(-4, 0.0, 4.0))
+        balls[0].color = Color.BLUE.toVec()
+        balls[1].color = Color.RED.toVec()
+        balls[2].color = Color.GREEN.toVec()
+        balls[3].color = Color.YELLOW.toVec()
+        balls[4].color = Color.ORANGE.toVec()
+        balls[5].color = Color.MAGENTA.toVec()
+        balls[6].color = Color.PINK.toVec()
+        anchors.forEach { it.color = Color.GRAY.toVec() }
+        balls.forEach { register(it) }
+        anchors.forEach { register(it) }
+        links.forEach { register(it) }
+    }
+}
+fun main() {
+    ImpulseSpheres().start()
+}
